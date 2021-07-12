a7471e9d2080f58d145a3b86f1f55eaf7d26431c
Pipe Otálvaro <afotalvaroc@uqvirtual.edu.co>
2021-07-11 23:37:00 -0500

    Vista para agregar clientes

---
+++ Views/clientes/agregar.php
@@ -0,0 +1,53 @@
+<div class="box-principal">
+    <h3 class="titulo">Agregar Cliente
+        <hr>
+    </h3>
+    <div class="panel panel-success">
+        <div class="panel-heading">
+            <h3 class="panel-title">Agregar un nuevo cliente</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+                    <form class="form-horizontal" action="" method="POST" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Nombre del cliente</label>
+                            <input class="form-control" name="nombre" type="text" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Primer Apellido</label>
+                            <input class="form-control" name="primerApellido" type="text" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Segundo Apellido</label>
+                            <input class="form-control" name="segundoApellido" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Telefono</label>
+                            <input class="form-control" name="promedio" type="number" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Email</label>
+                            <input class="form-control" name="promedio" type="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Dirección</label>
+                            <input class="form-control" name="promedio" type="text" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="inputEmail" class="control-label">Imagen</label>
+                            <input class="form-control" name="imagen" id="imagen" type="file" required>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">Registrar</button>
+                            <button type="reset" class="btn btn-warning">Borrar</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+    </div>
+</div>
